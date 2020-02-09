6ce4ea96cc153cc2b254d0ea05cb2847810740d9
zhouyang <zy_whynot@163.com>
2020-02-09 21:55:45 +0800

    创建模板文件夹、完善首页视图、实现网站图标视图

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,7 +1,13 @@
 from . import index_blu
+from flask import render_template, current_app
 
 
 # 给蓝图注册路由
 @index_blu.route('/')
 def index():
-    return 'index'
+    return render_template('news/index.html')
+
+
+@index_blu.route("/favicon.ico")
+def favicon():
+    return current_app.send_static_file('news/favicon.ico')
