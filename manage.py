6911790f02eccba59b5cd1e9c8a5b1e7900d0fcc
zhouyang <zy_whynot@163.com>
2020-02-09 18:50:48 +0800

    '数据表的分析与创建'

--- manage.py
+++ manage.py
@@ -1,15 +1,16 @@
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
-from info import create_app, db
+from info import create_app, db, models
 
 # 创建app，传入配置模式：development / production
 app = create_app("development")
 
-# 集成flask_script，可以在命令行添加参数
+# 集成flask_script，创建终端命令对象
 manager = Manager(app)
-# 数据库迁移
+# 使用数据库迁移类将应用和数据库连接对象关联起来
 Migrate(app, db)
+# 给终端命令对象添加数据库迁移命令
 manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
-    app.run()
+    manager.run()
