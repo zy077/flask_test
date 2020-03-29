ec90da13c328845af6262c07808ee53a3337a8eb
zhouyang <zy_whynot@163.com>
2020-03-29 18:40:33 +0800

    '实现新闻列表分类分页的后端逻辑'

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,7 +1,8 @@
 from info.models import User, News, Category
+from info.utils.response_code import RET
 from . import index_blu
-from flask import render_template, current_app, make_response, session
-from info.constants import CLICK_RANK_MAX_NEWS
+from flask import render_template, current_app, make_response, session, request, jsonify
+from info import constants
 
 
 # 给蓝图注册路由
@@ -28,7 +29,7 @@ def index():
     new_list = None
     try:
         # news = News.query.order_by('-clicks').limit(10)
-        new_list = News.query.order_by(News.clicks.desc()).limit(CLICK_RANK_MAX_NEWS)
+        new_list = News.query.order_by(News.clicks.desc()).limit(constants.CLICK_RANK_MAX_NEWS)
     except Exception as e:
         current_app.logger.error(e)
 
@@ -54,3 +55,48 @@ def index():
 @index_blu.route("/favicon.ico")
 def favicon():
     return current_app.send_static_file('news/favicon.ico')  # send_static_file是系统访问静态文件所调用的方法
+
+
+@index_blu.route('news_list')
+def get_news_list():
+    """
+    获取指定分类的新闻列表
+    :return:
+    """
+    # 1、获取参数
+    args_dict = request.args
+    page = args_dict.get("page", '1')
+    per_page = args_dict.get("per_page", constants.HOME_PAGE_MAX_NEWS)  # 默认显示10条数据
+    category_id = args_dict.get("cid", '1')
+
+    # 2、校验参数
+    try:
+        page = int(page)
+        per_page = int(per_page)
+        category_id = int(category_id)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify({"errno": RET.PARAMERR, "errmsg": "参数错误"})
+
+    # 3、查询数据并分页
+    filter = []
+    # 如果category_id不为1，则添加分类id过滤
+    if category_id != 1:
+        filter.append(News.category_id == category_id)
+
+    try:
+        paginate = News.query.filter(*filter).order_by(News.create_time.desc()).paginate(page, per_page, False)
+        # 获取当前页的新闻数据
+        news_list = paginate.items
+        # 获取总页数
+        total_page = paginate.pages
+        # 获取当前页
+        current_page = paginate.page
+
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify({"errno": RET.DBERR, "errmsg": "查询数据错误！"})
+
+    # 4、返回响应
+    return jsonify(errno=RET.OK, errmsg="OK", totalPage=total_page, currentPage=current_page, news_list=news_list,
+                   cid=category_id)
