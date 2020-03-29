9ab3db0b7ba5d13e22072a5d4facb77621b95f48
zhouyang <zy_whynot@163.com>
2020-03-29 17:23:21 +0800

    实现新闻分类的前后端逻辑

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,5 +1,4 @@
-from info.models import User
-from info.models import News
+from info.models import User, News, Category
 from . import index_blu
 from flask import render_template, current_app, make_response, session
 from info.constants import CLICK_RANK_MAX_NEWS
@@ -9,19 +8,6 @@ from info.constants import CLICK_RANK_MAX_NEWS
 @index_blu.route('/')
 @index_blu.route("/index")
 def index():
-    # 查询新闻数据，按点击量从大到小排行，只要前十条数据
-    new_list = None
-    try:
-        # news = News.query.order_by('-clicks').limit(10)
-        new_list = News.query.order_by(News.clicks.desc()).limit(CLICK_RANK_MAX_NEWS)
-    except Exception as e:
-        current_app.logger.error(e)
-
-    click_news_list = []
-    for new in new_list if new_list else []:
-        click_news_list.append(new.to_dict())
-
-
     # 判断用户是否登录
     user_id = session.get("user_id")
     try:
@@ -38,9 +24,29 @@ def index():
     # resp.set_cookie("csrf_token", csrf_token)
     # return resp
 
+    # 查询新闻数据，按点击量从大到小排行，只要前十条数据
+    new_list = None
+    try:
+        # news = News.query.order_by('-clicks').limit(10)
+        new_list = News.query.order_by(News.clicks.desc()).limit(CLICK_RANK_MAX_NEWS)
+    except Exception as e:
+        current_app.logger.error(e)
+
+    click_news_list = []
+    for new in new_list if new_list else []:
+        click_news_list.append(new.to_dict())
+
+    # 查询新闻分类数据
+    categories = Category.query.all()
+    category_list = []
+    # for category in enumerate(categories):
+    for category in categories:
+        category_list.append(category.to_dict())
+
     data = {
         "user_info": user.to_dict() if user else None,
-        "click_news_list": click_news_list
+        "click_news_list": click_news_list,
+        "categories": category_list
     }
     return render_template('news/index.html', data=data)
 
