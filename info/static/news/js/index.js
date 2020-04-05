0a17c8c36f73cce498164b9ee0b964ae67926c8a
zhouyang <zy_whynot@163.com>
2020-04-05 18:29:01 +0800

    '实现新闻列表页面滚动加载下一页的数据'

--- info/static/news/js/index.js
+++ info/static/news/js/index.js
@@ -1,7 +1,7 @@
 var currentCid = 1; // 当前分类 id
 var cur_page = 1; // 当前页
 var total_page = 1;  // 总页数
-var data_querying = true;   // 是否正在向后台获取数据
+// var data_querying = true;   // 是否正在向后台获取数据
 
 
 $(function () {
@@ -10,41 +10,53 @@ $(function () {
 
     // 首页分类切换
     $('.menu li').click(function () {
-        var clickCid = $(this).attr('data-cid')
+        // 记录点击选中的新闻分类id
+        var clickCid = $(this).attr('data-cid');
+
+        // 修改点击标签的选中样式
+        // （1）删除所有的分类标签样式
         $('.menu li').each(function () {
             $(this).removeClass('active')
-        })
-        $(this).addClass('active')
+        });
+        // （2）给点击分类标签添加选中样式
+        $(this).addClass('active');
 
-        if (clickCid != currentCid) {
-            // 记录当前分类id
-            currentCid = clickCid
+        // 判断点击分类标签是否和当前分类标签相同
+        if(clickCid != currentCid){
+            // 更新分类id
+            currentCid = clickCid;
 
-            // 重置分页参数
-            cur_page = 1
-            total_page = 1
+            // 重置当前页参数
+            cur_page = 1;
             updateNewsData()
         }
     })
 
     //页面滚动加载相关
     $(window).scroll(function () {
+        // 浏览器窗口高度（即滚动条高度）
+        var window_height = $(window).height();
 
-        // 浏览器窗口高度
-        var showHeight = $(window).height();
+        // 网页高度
+        var webpage_height = $(document).height();
 
-        // 整个网页的高度
-        var pageHeight = $(document).height();
+        // 已经滚动了多少，这个是随着页面滚动实时变化的
+        var scrolled_height = $(document).scrollTop();
 
-        // 页面可以滚动的距离
-        var canScrollHeight = pageHeight - showHeight;
+        // 还可以滚动的高度
+        var canscroll_height = webpage_height - (window_height + scrolled_height);
 
-        // 页面滚动了多少,这个是随着页面滚动实时变化的
-        var nowScroll = $(document).scrollTop();
+        // 可以滚动的高度小于100时，发起获取新数据的请求
+        if(canscroll_height < 100){
+            // 判断当前页是否小于总页数
+            if(cur_page < total_page){
+                // 获取下一页数据
+                cur_page += 1;
+                updateNewsData();
+            }
 
-        if ((canScrollHeight - nowScroll) < 100) {
-            // TODO 判断页数，去更新新闻数据
         }
+
     })
 })
 
@@ -58,8 +70,15 @@ function updateNewsData() {
     $.get('/news_list', param, function (resp) {
         if(resp.errno == '0'){
             console.log(resp.data);
-            // 清空原来的数据
-            $('.list_con').html('');
+            // 查询数据结束，修改标记是否正在向后台获取数据的变量：data_querying
+            // data_querying = false;
+            // 更新总页数标识
+            total_page = resp.data.totalPage;
+
+            // 当前页是第一页时，清空原来的数据
+            if(cur_page == 1){
+                $('.list_con').html('');
+            }
             // 渲染后端返回的数据
             for(var i=0; i<resp.data.news_list.length; i++){
                 var news = resp.data.news_list[i];
@@ -74,6 +93,10 @@ function updateNewsData() {
                 content += '</li>';
                 $('.list_con').append(content)
             }
+        }
+        else{
+            // 查询数据失败
+            alert('查询数据失败！')
         }
     })
 }
